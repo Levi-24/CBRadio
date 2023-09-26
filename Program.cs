14846f40400dc22d7394092a3c27c2313c33b3f5
Levente Major <leventemajor36@gmail.com>
2023-09-26 08:25:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Radio> radio = new List<Radio>();
+            using StreamReader sr = new StreamReader(
+                path: "../../../src/cb.txt",
+                Encoding.UTF8
+                );
+
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream) radio.Add(new Radio(sr.ReadLine()));
+
+            Console.WriteLine($"3.Feladat: Bejegyzések száma:{radio.Count}");
+
+            bool f4 = radio.Any(h => h.AdasDb == 4);
+            Console.WriteLine($"4.Feladat: {(f4 ?"volt":"nem volt")} 4 adást indító sofőr");
+        }
+    }
+}
---
+++ Radio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    class Radio
+    {
+        public TimeSpan OraPerc { get; set; }
+        public int AdasDb { get; set; }
+        public string Nev { get; set; }
+
+        public Radio(string beolvasottSor)
+        {
+            var darabok = beolvasottSor.Split(';');
+            OraPerc = new TimeSpan(int.Parse(darabok[0]), int.Parse(darabok[1]), 0);
+            AdasDb = int.Parse(darabok[2]);
+            Nev = darabok[3];
+        }
+    }
+}
