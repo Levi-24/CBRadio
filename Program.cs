7466239eccba9c07bd93e70d3819de1df2673e6f
Levente Major <leventemajor36@gmail.com>
2023-09-26 12:07:32 +0200

    refined

--- Program.cs
+++ Program.cs
@@ -42,20 +42,23 @@ namespace ConsoleApp53
                 writer.WriteLine($"{Convert.ToString(AtszamolPercbe(item.OraPerc))};{item.Nev};{item.AdasDb}");
             }
 
-            var f8 = radio.GroupBy(h => h.Nev).Count();
-            Console.WriteLine($"8.Feladat: Sofőrök száma: {f8} fő");
+            //var f8 = radio.GroupBy(h => h.Nev).Count();
+            //Console.WriteLine($"8.Feladat: Sofőrök száma: {f8} fő");
 
-            //var f9 = radio.Max(h => h.AdasDb);
-            //string f9n = "";
-            //foreach (var item in radio)
-            //{
-            //    if (item.AdasDb == f9) f9n = item.Nev;
-            //}
+            var f8dic = radio
+                .GroupBy(h => h.Nev)
+                .ToDictionary(n => n.Key, hsz => hsz.Sum(h => h.AdasDb));
+            Console.WriteLine($"8.f: soforok szama: {f8dic.Count} fo");
 
-            var f9 = radio.OrderBy(kvp => kvp.AdasDb).Last();
-            Console.WriteLine("9.Feladat: Legtöbb adást indító sofőr");
-            Console.WriteLine($"\t Név: {f9.Nev}");
-            Console.WriteLine($"\t Adások száma: {f9.AdasDb} alkalom");
+            var f9 = f8dic.OrderBy(kvp => kvp.Value).Last();
+            Console.WriteLine("9.f.: legtobb adast indito sofor:");
+            Console.WriteLine($"\tnev: {f9.Key}");
+            Console.WriteLine($"\tadasok szama: {f9.Value} alkalom");
+
+            //var f9 = radio.OrderBy(kvp => kvp.AdasDb).Last();
+            //Console.WriteLine("9.Feladat: Legtöbb adást indító sofőr");
+            //Console.WriteLine($"\t Név: {f9.Nev}");
+            //Console.WriteLine($"\t Adások száma: {f9.AdasDb} alkalom");
         }
         static int AtszamolPercbe(TimeSpan oraPerc)
         {
